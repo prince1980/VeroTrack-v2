72da6c96719f7d244d1346381079095b2f22f79b
VeroTrack Automated <verotrack@example.com>
2026-04-05 12:04:20 +0530

    Bump SW cache to force mobile refresh

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Bump CACHE when you change CSS/JS so phones pick up updates. */
-const CACHE = 'verotrack-v2026-04-05-7';
+const CACHE = 'verotrack-v2026-04-05-8';
 
 function scopeUrl(path) {
   return new URL(path, self.registration.scope).href;
